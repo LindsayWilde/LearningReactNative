f43f4562d7cd0ae20821c4dd0275f15326cdf704
LindsayWilde <lindsaywilde439@gmail.com>
2022-03-16 14:18:28 -0600

    Hello World

---
+++ PracticeUI1/App.js
@@ -0,0 +1,24 @@
+//@ts-check
+
+//import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/stack';
+import { Text } from 'react-native';
+
+
+//const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <>
+      <Text>
+        Hello World!  
+      </Text>    
+    </>
+  );
+};
+
+
+export default App;
